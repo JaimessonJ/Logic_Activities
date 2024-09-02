a8241e23720882e7ce2bc84688cfce97d689620b
Jaimeson Alejandro Quintero Macias <aquinteromacias.rar@gmail.com>
2024-09-02 17:43:12 -0500

    Guardar Avances

--- Excersice1_Coordinates/Excersice1_Coordinates/Program.cs
+++ Excersice1_Coordinates/Excersice1_Coordinates/Program.cs
@@ -1,7 +1,16 @@
 ﻿internal class Program
+    
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Lea las coordenadas de dos puntos y que muestre la distacia entre los puntos.
+
+        //Datos de entrada
+        //Declarar variables en este caso consta de 4
+        double x1, x2 , y1 , y2 d;
+
+        //Vamos a inicializar las variables pidiendo sus datos por pantalla
+        Console.WriteLine("Ingrese la coordenada de x1: ");
+        x1 = Convert.ToDouble(Console.ReadLine()); //Esta es la forma de escribir en pantalla
     }
 }
